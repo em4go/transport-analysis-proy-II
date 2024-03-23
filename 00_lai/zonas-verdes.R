3f4afdd1041d79ab47ba592653f0303d65367e00
LaiqianDS <ji.laiqian@gmail.com>
2024-03-23 19:22:23 +0100

    Updated calculation of green spaces

--- 00_lai/zonas-verdes.R
+++ 00_lai/zonas-verdes.R
@@ -1,66 +1,118 @@
-getwd()
-file <- "./00_lai/zonas-verdes.csv"
-
-df <- read.csv2(file=file)
-df$m2_poligon <- round(as.numeric(df$m2_poligon), 0)
-
-summary(df$m2_poligon)
-table(df$m2_poligon)
-
-sum(df$m2_poligon)/807.693
-names(df)
-df$geo_point_2d
-
 library(tidyverse)
 library(ggplot2)
 library(leaflet)
-
-# Separa la columna 'geo_point_2d' en dos columnas: 'latitud' y 'longitud'
-df <- separate(df, geo_point_2d, into = c("latitud", "longitud"), sep = ",", convert = TRUE)
-
-# Convertimos las columnas a tipo numérico si no se han convertido automáticamente
-df$latitud <- as.numeric(df$latitud)
-df$longitud <- as.numeric(df$longitud)
-
-# Calcula el centro del mapa
-centro_lat <- mean(df$latitud)
-centro_lon <- mean(df$longitud)
-
-# Crea el mapa Leaflet centrado en el área de los puntos geográficos
-mapa <- leaflet(data = df) %>%
-  addTiles() %>%
-  addMarkers(~longitud, ~latitud) %>%
-  setView(lng = centro_lon, lat = centro_lat, zoom = 10)
-
-# Muestra el mapa
-mapa
-
-sum(df$m2_poligon)
-
 library(jsonlite)
 library(sf)
 library(geojsonsf)
 library(geojsonio)
+library(purrr)
+library(igraph)
+library(osmdata)
 
-# cojo un geoshape de prueba
-df$geo_shape
-p <- df$geo_shape[1]
-sf <- geojson_sf(p)
-st_area(sf)
+getwd()
 
-geojson_sf(df$geo_shape)
+# Carga el archivo CSV con los datos de las zonas verdes
+file <- "./00_lai/zonas-verdes.csv"
+df <- read.csv2(file=file)
+df$m2_poligon <- round(as.numeric(df$m2_poligon), 0)
 
-summary(df$geo_shape)
+names(df)
+head(df)
 
+# Quito las columnas que contienen "m2zv"
+df <- df[ , !grepl("m2zv", names(df))]
 
+head(df)
+
+summary(df$m2_poligon)
+names(df)
+
+df$geo_point_2d
+
+sum(df$m2_poligon)
+
+# Comprobamos si hay algún string vacío en la columna 'geo_shape'
 hay_string_vacio <- any(grepl("^$", df$geo_shape))
 print(hay_string_vacio)
 
 df <- subset(df, !grepl("^$", geo_shape))
 
+# Comprobamos si sigue habiendo algún string vacío en la columna 'geo_shape'
 hay_string_vacio <- any(grepl("^$", df$geo_shape))
-df$geo_shape <- geojson_sf(df$geo_shape)
-df$area <- st_area(df$geo_shape)
-df$area
+print(hay_string_vacio)
+
+# Convertimos la columna 'geo_shape' a tipo 'sf'
+df$geo_shape <- map(df$geo_shape, geojson_sf)
+
+# Calculamos el área de cada polígono
+lista_area <- map(df$geo_shape, st_area)
+df$area <- unlist(lista_area)
+df$area <- round(df$area, 0)
+
+# Redondeo a la decena por abajo
+df$area <- floor(df$area / 10) * 10
+
+head(df)
+names(df)
 sum(df$area)
 sum(df$m2_poligon)
+
+# Ahora, vamos a meter cada polígono en un barrio mediante su geo_point
+
+obtener_poligonos <- function(df){
+  polygons <- lapply(barrios$geo_shape, function(x) st_read(x, quiet = TRUE))
+  
+  # Combina todos los objetos sf en un solo objeto sf
+  polygons_barrios <- do.call(rbind, polygons)
+  
+  polygons_barrios$Nombre <- barrios$Nombre
+  return(polygons_barrios)
+}
+
+# Cargamos los datos de los barrios
+barrios <- read.csv2("data/barrios_valencia.csv", sep = ";")
+polygons_barrios <- obtener_poligonos(barrios)
+polygons_barrios
+
+# Cuento los distintos barrios que hay
+s_barrios <- unique(polygons_barrios$Nombre)
+class(s_barrios)
+
+# Mediante la columna geo_point_2d, vamos a asignar cada polígono a un barrio
+
+# Separo geo_point_2d en dos columnas: x e y.
+# Debo hacer un split 
+cords <- strsplit(df$geo_point_2d, ",")
+df$x <- sapply(cords, function(x) x[1])
+df$y <- sapply(cords, function(x) x[2])
+df$x <- as.numeric(df$x)
+df$y <- as.numeric(df$y)
+
+head(df)
+
+for (zv in 1:nrow(df)) {
+  y <- df$x[zv]
+  x <- df$y[zv]
+  point <- st_point(c(x, y))
+  # b devuelve una lista con el índice del polígono en el que está el nodo
+  b <- unlist(st_within(point, polygons_barrios$geometry)) 
+
+  # Si el nodo está en un polígono, asigno el nombre del barrio correspondiente
+  if (length(b) > 0) {
+    df$barrio[zv] <- polygons_barrios$Nombre[b]
+  } else {
+    df$barrio[zv] <- 'Otro'
+  }
+}
+
+s_barrios2 <- c(unique(df$barrio))
+
+# Obtengo la diferencia de los dos vectores para saber qué barrios no tienen zonas verdes
+setdiff(s_barrios, s_barrios2)
+
+# Obtengo un data frame resumen con la suma de las áreas de las zonas verdes por barrio
+df_resumen <- df %>% group_by(barrio) %>% summarise(area = sum(area))
+sum(df_resumen$area)
+
+# Guardo el data.frame en un csv
+write.csv2(df_resumen, "data/zonas_verdes_por_barrio.csv", row.names = FALSE)
