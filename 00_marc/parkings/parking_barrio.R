54bab349c845e61cb7d2957c2ffa08f45c74e047
Marxx01 <marc.hurtado@gmail.com>
2024-03-27 11:22:59 +0100

    info_barrios

---
+++ 00_marc/parkings/parking_barrio.R
@@ -0,0 +1,6 @@
+data_barrios <- read.csv2("./data/barrios_valencia.csv", sep = ";")
+
+data_mob_red <- read.csv("./00_marc/dataTratada/plazas_mobred_geo.csv", sep = ",")
+  
+data_parkings <- read.csv("./00_marc/dataTratada/dataParkings.csv", sep = ",")
+
